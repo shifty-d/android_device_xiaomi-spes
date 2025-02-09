1574f24fbc385e0e107bec5401d0ee3cd3c12f29
HALt <102590462+halt-spesn@users.noreply.github.com>
2025-02-09 23:52:31 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo "Syncing additional trees"
 rm -rf hardware/xiaomi/megvii
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-18.1
+git clone https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi -b twelve
 git clone https://github.com/halt-spesn/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/spes --depth=1
 git clone https://github.com/halt-spesn/vendor_xiaomi_miui_camera vendor/xiaomi/camera --depth=1
